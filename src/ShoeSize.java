1b8bcd6114fc203c5a17a62a89076e218f76a433
chen <lycbe31@hotmail.com>
2018-04-30 12:34:07 +1000

    txt

--- src/ShoeSize.java
+++ src/ShoeSize.java
@@ -1,20 +1,3 @@
-
-/* ShoeSize - Eric McCreath 2015 - GPL
- * This class stores a persons shoe size.
- */
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 
 public class ShoeSize {
@@ -48,53 +31,30 @@ public class ShoeSize {
     static ShoeSize load() {
         // add code here that will load shoe size from a file called "FILENAME"
         ShoeSize ss = new ShoeSize();
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db;
         try{
-            db = dbf.newDocumentBuilder();
-            Document doc = db.parse(new File("1.xml"));
-            Node shorsize = doc.getFirstChild();
-            NodeList nl = shorsize.getChildNodes();
-            Node n = nl.item(0);
-            if(n != null){
-                ss.shoesize = Integer.parseInt(n.getTextContent());
+            BufferedReader b_reader = new BufferedReader(new FileReader("bespoke.txt"));
+            String line = b_reader.readLine();
+            if (line != null){
+                //System.out.println(11111);
+                int shoesize = Integer.parseInt(line);
+                ss.shoesize = shoesize;
             }
-
-        }catch(Exception e){
-            System.err.println("Problem loading " + "1.xml");
+        }catch (IOException x){
+            System.err.format("IOException: %s%n", x);
         }
-
-
         return ss;
     }
 
     void save() {
         // add code here that will save shoe size into a file called "FILENAME"
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db;
         try{
-            db = dbf.newDocumentBuilder();
-            Document doc = db.newDocument();
-            Element shoe = doc.createElement("SHOE");
-
-            Element size = doc.createElement("SIZE");
-            size.appendChild(doc.createTextNode(String.valueOf(shoesize)));
-            shoe.appendChild(size);
-            doc.appendChild(shoe);
-            TransformerFactory transformerFactory = TransformerFactory
-                    .newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-
-            // set xml encoding to utf-8
-            transformer.setOutputProperty(OutputKeys.ENCODING,"utf-8");
-
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("1.xml"));
-            transformer.transform(source, result);
-
-
-        }catch(Exception e){
-            System.err.println("Problem saving " + "1.xml");
+            BufferedWriter b_writer = new BufferedWriter(new FileWriter("bespoke.txt"));
+            if(shoesize != null){
+                b_writer.write(shoesize.toString());
+            }
+            b_writer.close();
+        }catch (IOException x){
+            System.err.format("IOException: %s%n", x);
         }
     }
 }
